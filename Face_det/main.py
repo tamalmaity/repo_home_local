4281ee26ef25451b74621f076802f38b94af9234
Tamal Maity <tamaldeepmaity@gmail.com>
2019-11-17 12:43:49 +0530

    Good old face and eye detector using opencv

---
+++ Face_det/main.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier ('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier ('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (fx,fy,fw,fh) in faces:
+        cv2.rectangle(img, (fx,fy), (fx+fw, fy+fh), (255,0,0), 2)
+
+        face_roi_gray = gray[fy:int((fy+fh)*0.5), fx:fx+fw]     #Since eyes will be inside face and at top half of face
+                                                                # (hopefully :P )
+        face_roi_img = img[fy:int((fy+fh)*0.5), fx:fx+fw]
+
+        eyes = eye_cascade.detectMultiScale(face_roi_gray)
+
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(face_roi_img, (ex, ex+ew), (ey, ey+eh), (0,0,255), 2)
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) and 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
